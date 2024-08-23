9f9cfec7bd3c880cd12ed7882900f5b3c43c34b3
hFire24 <51500563+hFire24@users.noreply.github.com>
2024-08-23 14:04:12 -0400

    & only applies to Google and YouTube

--- background.js
+++ background.js
@@ -118,7 +118,7 @@ function saveBlockedUrl(url, patterns, reason) {
       savedUrls[today] = [];
     }
 
-    if (url.includes('&'))
+    if (url.includes('&') && (url.includes('google') || url.includes('youtube')))
       url = url.slice(0, url.indexOf('&'));
 
     let urlEntry = savedUrls[today].find(entry => entry.url === url);
